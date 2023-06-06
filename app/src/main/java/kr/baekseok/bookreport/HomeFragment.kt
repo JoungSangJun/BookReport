7b95cf0970f365efcd56c1f14e913f60b504d097
JoungSangJun <qlz8674502@naver.com>
2023-06-06 10:30:37 +0900

    feat : HomeFragment add recyclerView
    HomeFragment에 recyclerView 장착

--- app/src/main/java/kr/baekseok/bookreport/HomeFragment.kt
+++ app/src/main/java/kr/baekseok/bookreport/HomeFragment.kt
@@ -2,9 +2,6 @@ package kr.baekseok.bookreport
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,23 +9,25 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kr.baekseok.adapter.ReportRecyclerAdapter
+import kr.baekseok.addreport.BookReportApplication
+import kr.baekseok.addreport.ReportAddActivity
 import kr.baekseok.bookreport.databinding.FragmentHomeBinding
-import kr.baekseok.data.BooksInfoRepository
-import kr.baekseok.data.DefaultAppContainer
-import kr.baekseok.register.RegisterActivity
-import kr.baekseok.viewmodel.BooksUiState
+import kr.baekseok.room.BookReportDao
+import kr.baekseok.room.BookReportData
 import kr.baekseok.viewmodel.HomeViewModel
 
 // viewModel에 매개변수 전달을 위한 ViewModelFactory 생성
-class HomeViewModelFactory(private val booksInfoRepository: BooksInfoRepository) :
+class HomeViewModelFactory(private val booksReportDao: BookReportDao) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            return HomeViewModel(booksInfoRepository) as T
+            return HomeViewModel(booksReportDao) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
@@ -38,12 +37,13 @@ class HomeFragment : Fragment() {
 
     private lateinit var hBinding: FragmentHomeBinding
     lateinit var homeViewModel: HomeViewModel
+    private lateinit var reportAdapter: ReportRecyclerAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         hBinding = FragmentHomeBinding.inflate(inflater, container, false)
         val homeViewModelFactory =
-            HomeViewModelFactory(booksInfoRepository = DefaultAppContainer().booksPhotosRepository)
+            HomeViewModelFactory(booksReportDao = (requireActivity().application as BookReportApplication).bookReportDatabase.bookReportDao())
         homeViewModel = ViewModelProvider(this, homeViewModelFactory).get(HomeViewModel::class.java)
 
         val mAuth: FirebaseAuth = Firebase.auth
@@ -56,22 +56,25 @@ class HomeFragment : Fragment() {
             }
         }
 
+        setupRecyclerView()
 
-        hBinding.rectangle2.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // 텍스트 변경 중에 호출됩니다.
-                homeViewModel.getBooksInfo(s.toString())
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                // 텍스트 변경 후에 호출됩니다.
-            }
-        })
+//        hBinding.rectangle2.addTextChangedListener(object : TextWatcher {
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            }
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                // 텍스트 변경 중에 호출됩니다.
+//                homeViewModel.getBooksInfo(s.toString())
+//            }
+//
+//            override fun afterTextChanged(s: Editable?) {
+//                // 텍스트 변경 후에 호출됩니다.
+//            }
+//        })
 
         hBinding.fab.setOnClickListener {
+            homeViewModel.getAllBooksReport()
             val context = requireContext()
             val intent = Intent(context, ReportAddActivity::class.java)
             startActivity(intent)
@@ -80,6 +83,21 @@ class HomeFragment : Fragment() {
         autoLogin()
 
         return hBinding.root
+    }
+
+    private fun setupRecyclerView() {
+        val recyclerView = hBinding.reportRecyclerView
+        reportAdapter =
+            ReportRecyclerAdapter(
+                requireContext(),
+                homeViewModel.reportUiState.value ?: listOf(
+                    BookReportData(0, "hi", "hi"),
+                    BookReportData(0, "hi", "hi")
+                ),
+                LayoutInflater.from(requireContext())
+            )
+        recyclerView.adapter = reportAdapter
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
     }
 
     fun autoLogin() {
