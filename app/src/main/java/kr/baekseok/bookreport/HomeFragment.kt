0c9bdee24f81cbc60600bcdf221e3f790ebdd8fc
hangu26 <125523867+hangu26@users.noreply.github.com>
2023-06-10 19:22:36 +0900

    commit
    홈 프래그먼트 nullpointException 수정

--- app/src/main/java/kr/baekseok/bookreport/HomeFragment.kt
+++ app/src/main/java/kr/baekseok/bookreport/HomeFragment.kt
@@ -52,13 +52,12 @@ class HomeFragment : Fragment() {
             HomeViewModelFactory(booksReportDao = (requireActivity().application as BookReportApplication).bookReportDatabase.bookReportDao())
         homeViewModel = ViewModelProvider(this, homeViewModelFactory).get(HomeViewModel::class.java)
 
-        val mAuth: FirebaseAuth = Firebase.auth
-        val user: FirebaseUser = mAuth.currentUser!!
+        val mAuth : FirebaseAuth = Firebase.auth
+        val user = mAuth.currentUser
 
-        user.getIdToken(true).addOnCompleteListener {//자동 로그인
+        user?.getIdToken(true)?.addOnCompleteListener {//자동 로그인
             if (it.isSuccessful) {
                 var idToken: String = it.getResult().token!!
-                Toast.makeText(activity, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
             }
         }
 
